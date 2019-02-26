a9df390c34ad615529bce92a94f567a7acaa7ea1
avcopan <avcopan@gmail.com>
2019-02-26 17:48:26 -0600

    Fix imports for constructors and readers

--- automol/constructors/__init__.py
+++ automol/constructors/__init__.py
@@ -1,2 +1,12 @@
 """ constructors for each internal datatype
 """
+from . import geom
+from . import graph
+from . import zmatrix
+
+
+__all__ = [
+    'geom',
+    'graph',
+    'zmatrix',
+]
--- automol/readers/__init__.py
+++ automol/readers/__init__.py
@@ -1,2 +1,7 @@
 """ re patterns and helper functions for reading data-types from strings
 """
+from . import zmatrix
+
+__all__ = [
+    'zmatrix',
+]
