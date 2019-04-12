c8c373c57445af3a552148929805fe7052bc4d05
avcopan <avcopan@gmail.com>
2019-04-12 13:35:30 -0500

    Slight refactor in zmatrix.tors; Fixes python 2 compatibility

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 
 setup(name="autochem",
-      version="0.1.12",
+      version="0.1.13",
       packages=["automol",
                 "automol.constructors",
                 "automol.cart",
