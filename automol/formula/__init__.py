fb09d274664255857e00a3a1615a340790b797af
Stephen Klippenstein <sjk@anl.gov>
2019-09-13 13:45:25 -0500

    added electron_count to init

--- automol/formula/__init__.py
+++ automol/formula/__init__.py
@@ -1,6 +1,7 @@
 """ molecular formula
 """
 from automol.formula._formula import atom_count
+from automol.formula._formula import electron_count
 from automol.formula._formula import hydrogen_count
 from automol.formula._formula import add_element
 from automol.formula._formula import add_hydrogen
@@ -10,6 +11,7 @@ from automol.formula import reac
 
 __all__ = [
     'atom_count',
+    'electron_count',
     'hydrogen_count',
     'add_element',
     'add_hydrogen',
