21c3d32868bcc4bdb37d24ecabe0f359cb6028f0
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2020-04-14 19:17:40 -0500

    update geom.py, zmat/ts.py and distribution stuff

--- automol/geom.py
+++ automol/geom.py
@@ -47,6 +47,22 @@ def coordinates(geo, angstrom=False):
     return xyzs
 
 
+def count(geo):
+    """ count the number of atoms in the geometry
+    """
+    return len(symbols)
+
+
+def heavy_count(geo):
+    """ count the number of atoms in the geometry
+    """
+    cnt = 0
+    for sym in symbols(geo):
+        if sym != 'H':
+            cnt += 1
+    return cnt
+
+
 # validation
 def is_valid(geo):
     """ is this a valid geometry?
@@ -349,7 +365,6 @@ def reflect_coordinates(geo, idxs, axes):
     geo_reflected = set_coordinates(geo, reflect_dct)
 
     return geo_reflected
-
 
 
 def rot_permutated_geoms(geo, saddle=False, frm_bnd_key=[], brk_bnd_key=[], form_coords=[]):
