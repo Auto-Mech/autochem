e4561a501ffa4997e9bc1e660aa314d8e49f2fb1
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2020-05-01 00:49:19 -0500

    update yml files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 
 setup(name="autochem",
-      version="0.2.9",
+      version="0.1.0",
       packages=["automol",
                 "automol.cart",
                 "automol.convert",
