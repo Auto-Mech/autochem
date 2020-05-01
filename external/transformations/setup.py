60c074956ac43df7b5fd718dfdcc61864dc37c94
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2020-05-01 01:37:11 -0500

    update yaml, changed transformations import

--- external/transformations/setup.py
+++
@@ -1,9 +0,0 @@
-""" Install transformations
-"""
-from distutils.core import setup
-
-
-setup(name="transformations",
-      version="2019.2.20",
-      author="Christoph Gohlke",
-      packages=["transformations"])
---
+++ transformations/__init__.py
@@ -0,0 +1,11 @@
+"""
+  Python package "transformations" developed externally by Christoph Gohlke
+  See README and _transformations.py for more details
+"""
+
+from transformations._transformations import rotation_matrix
+
+
+__all__ = [
+    'rotation_matrix'
+]
