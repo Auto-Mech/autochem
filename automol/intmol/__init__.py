db311b4ee095c104d297c86cec24195823582917
Kevin Moore <kmoore@blueslogin3.lcrc.anl.gov>
2020-07-11 22:48:32 -0500

    update

--- automol/intmol/__init__.py
+++ automol/intmol/__init__.py
@@ -2,15 +2,15 @@
   Libraries for calculating inter- and intramolecular interactions
 """
 
-from automol.intmol._pot import ljpotential
-from automol.intmol._pot import exp6potential
+from automol.intmol._pot import lj_potential
+from automol.intmol._pot import exp6_potential
 from automol.intmol._pot import pairwise_potential_matrix
 from automol.intmol._rep import low_repulsion_struct
 
 
 __all__ = [
-    'ljpotential',
-    'exp6potential',
+    'lj_potential',
+    'exp6_potential',
     'pairwise_potential_matrix',
     'low_repulsion_struct'
 ]
