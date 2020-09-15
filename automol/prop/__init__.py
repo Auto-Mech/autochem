809cf10fba4128331ab25fde4e0d6779cc0dfb7b
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2020-09-15 15:00:55 -0500

    add functions for molecular property calcualtions

---
+++ automol/prop/__init__.py
@@ -0,0 +1,12 @@
+"""
+  Libraries for handling molecular property calculations
+"""
+
+from automol.prop._prop import total_dipole_moment
+from automol.prop._prop import total_polarizability
+
+
+__all__ = [
+    'total_dipole_moment',
+    'total_polarizability'
+]
