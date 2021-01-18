a3806d0a26499aade0e99f2e9e0465e76c26b984
avcopan <avcopan@gmail.com>
2021-01-18 12:42:33 -0500

    Makes getting torsion coordinate names nicer

--- automol/zmat/_rot.py
+++ automol/zmat/_rot.py
@@ -1,4 +1,4 @@
-""" functions for working with rotational bonds and groups
+""" functions for working with torsion axis (rotational bond)s and groups
 """
 import automol.convert.zmat
 import automol.graph
@@ -7,12 +7,34 @@ from automol.zmat._zmat import name_matrix
 from automol.zmat._zmat import string
 
 
-def torsion_leading_atom(zma, key1, key2):
-    """ leading atom for a torsion coordinate about a rotational bond
+def torsion_axes(zma):
+    """ get the torsion axes (rotational bonds) for a z-matrix
+    """
+    gra = automol.convert.zmat.graph(zma)
+    tors_axes = tuple(sorted(
+        map(tuple, map(sorted, automol.graph.rotational_bond_keys(gra)))))
+    return tors_axes
+
+
+def torsion_coordinate_name(zma, key1, key2):
+    """ name for a torsion coordinate about a torsion axis (rotational bond)
 
     :param zma: the z-matrix
-    :param key1: the first key in the rotational bond
-    :param key2: the second key in the rotational bond
+    :param key1: the first key in the torsion axis (rotational bond)
+    :param key2: the second key in the torsion axis (rotational bond)
+    """
+    key = torsion_leading_atom(zma, key1, key2)
+    name_mat = name_matrix(zma)
+    name = name_mat[key][-1]
+    return name
+
+
+def torsion_leading_atom(zma, key1, key2):
+    """ leading atom for a torsion coordinate about a torsion axis
+
+    :param zma: the z-matrix
+    :param key1: the first key in the torsion axis (rotational bond)
+    :param key2: the second key in the torsion axis (rotational bond)
 
     The leading atom is the atom whose dihedral defines the torsional
     coordinate, which must always be the first dihedral coordinate for this
@@ -43,46 +65,21 @@ def torsion_leading_atom(zma, key1, key2):
     return lead_key
 
 
-def torsion_coordinate_name(zma, key1, key2):
-    """ name for a torsion coordinate about a rotational bond
-
-    :param zma: the z-matrix
-    :param key1: the first key in the rotational bond
-    :param key2: the second key in the rotational bond
-    """
-    key = torsion_leading_atom(zma, key1, key2)
-    name_mat = name_matrix(zma)
-    name = name_mat[key][-1]
-    return name
-
-
-# def torsion_coordinates(zma):
-#     """ torsion coordinates for this z-matrix
-#     """
-#     gra = automol.convert.zmat.graph(zma)
-#     rot_bnd_keys = automol.graph.rotational_bond_keys(gra)
-#
-#     key_mat = key_matrix(zma)
-#     for row in key_mat:
-#         if frozenset
-#     print(automol.graph.string(gra, one_indexed=False))
-
-
 if __name__ == '__main__':
     import automol
     ICH = automol.smiles.inchi('C=C(CC)CC=CC(O)(N)CC1CCC1')
     GEO = automol.inchi.geometry(ICH)
     ZMA = automol.convert.geom.zmatrix_x2z(GEO)
     GEO = automol.convert.zmat.geometry(ZMA)
+
+    # Yuri's code:
     NAMES = automol.geom.zmatrix_torsion_coordinate_names(GEO)
     COO_DCT = automol.vmat.coordinates(ZMA)
     COOS = [x[0] for x in map(COO_DCT.__getitem__, NAMES)]
     print(automol.zmat.string(ZMA, one_indexed=False))
-    # print(torsion_coordinates(ZMA))
-    print(COOS)
-    GRA = automol.geom.graph(GEO)
-    ROT_BND_KEYS = sorted(map(sorted, automol.graph.rotational_bond_keys(GRA)))
 
-    TORS_NAMES = tuple(torsion_coordinate_name(ZMA, *k) for k in ROT_BND_KEYS)
+    # My code
+    TORS_AXES = torsion_axes(ZMA)
+    TORS_NAMES = tuple(torsion_coordinate_name(ZMA, *a) for a in TORS_AXES)
     print(TORS_NAMES)
     print(TORS_NAMES == NAMES)
