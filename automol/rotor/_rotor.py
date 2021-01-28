d21ecc5f8ff5ab4317ac1a8751e36bddbd4ad5ca
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2021-01-27 20:48:00 -0600

    update rotor and tests

--- automol/rotor/_rotor.py
+++ automol/rotor/_rotor.py
@@ -2,9 +2,12 @@
  Build the rotor and torsion objects
 """
 
+import numpy
 import automol
 from automol.rotor._par import TorsionParam
 
+
+# add a grouping function for the rotor
 
 class Rotor:
     """ Describes a Rotor
@@ -13,12 +16,8 @@ class Rotor:
     def __init__(self, zma, names=None, model=None, rotor_dct=None):
         """ constructor
         """
-        self.names = names
-        self.zma = zma
-
-        torsions = ()
-        for name in names:
-
+        torsion_axes = all_torsion_axes(zma, names=names)
+        print(_torsion_bond_keys(zma))
     # Use input rotor dct to construct all te torsion objects if there
     # else
     # Build the dict of tors  names {name: bnd_idx}
@@ -37,66 +36,58 @@ class Torsion:
         self.name = name
         self.zma = zma
         if tors_dct is not None:
-            self.symmetry = tors_dct.get(TorsionParam.ZMA, None)
-            self.span = tors_dct.get(TorsionParam.ZMA, None)
-            self.axis = tors_dct.get(TorsionParam.ZMA, None)
-            self.groups = tors_dct.get(TorsionParam.ZMA, None)
+            self.symmetry = tors_dct.get(TorsionParam.SYMMETRY, None)
+            self.span = tors_dct.get(TorsionParam.SPAN, None)
+            self.axis = tors_dct.get(TorsionParam.AXIS, None)
+            self.groups = tors_dct.get(TorsionParam.GROUPS, None)
         else:
-            self.symmetry = symmetry_number(self)
-            self.span = span(self)
-            self.axis, self.groups = rotational_groups(self)
+            pass
+            # self.symmetry = symmetry_number(self)
+            # self.span = span(self)
+            # self.axis, self.groups = rotational_groups(self)
 
 
-def torsion_axis():
-
-    lin_keys = sorted(gdummy_key_dct.keys())
-    gbnd_keys = automol.graph.rotational_bond_keys(ggra, lin_keys=lin_keys)
-    assert len(gbnd_keys) == len(bnd_keys)
-
-    axes = sorted(map(sorted, gbnd_keys))
-
-
-def torsion_groups(gra, axis=None):
-
-    if axis is None:
-        axis = torsion_axis()
-
-    return automol.graph.rotational_groups(ggra, axis)
-
-
-def torsion_symmetry(gra, axis=None):
-    """ Obtain the symmetry number for the torsion
-    """
-
-    if axis is None:
-        axis = torsion_axis()
-
-    return automol.graph.rotational_symmetry_number(
-        ggra, axis, lin_keys=lin_keys)
-
-
-def span(sym_num):
-    """
-    """
-    return (2.0 * numpy.pi) / sym_num
-
-
-def _torsion_bond_keys(zma, tors_name):
+def build_torsion_axes(zma, names=None):
     """ Generate the bond keys for the torsion
 
         or just get the torsion names and keys?
-        build a dictionary (build full dct for all tors in rotor
-                            split dcts into subdcts for groupings (including single torsion)
+        build a dictionary
+        (build full dct for all tors in rotor
+         split dcts into subdcts for groupings, including single torsion)
     """
 
     geo, gdummy_key_dct = automol.convert.zmat.geometry(zma)
     gra = automol.geom.graph(geo)
 
     lin_keys = sorted(gdummy_key_dct.keys())
-    gbnd_keys = automol.graph.rotational_bond_keys(ggra, lin_keys=lin_keys)
+    tors_keys = automol.graph.rotational_bond_keys(gra, lin_keys=lin_keys)
 
-    # names = {automol.zmat.torsion_coordinate_name(zma, *k) for k in bnd_keys}
-    for keys in gbnd_keys:
-        name = automol.zmat.torsion_coordinate_name(zma, keys)
+    # Determine all of the torsion names, take subset if requested
+    tors_names = tuple(automol.zmat.torsion_coordinate_name(zma, *keys)
+                       for keys in tors_keys)
+    if names is not None:
+        tors_names = tuple(tors_names for name in tors_names
+                           if name in names)
+
+    return dict(zip(tors_names, tors_keys))
 
 
+def torsion_groups(gra, axis):
+    """ Generate torsion groups
+    """
+    return automol.graph.rotational_groups(gra, *axis)
+
+
+def torsion_symmetry(gra, axis, lin_keys):
+    """ Obtain the symmetry number for the torsion
+    """
+    return automol.graph.rotational_symmetry_number(
+        gra, *axis, lin_keys=lin_keys)
+
+
+def torsion_span(gra, axis, lin_keys):
+    """ Obtain the torsional span
+    """
+    sym_num = automol.graph.rotational_symmetry_number(
+        gra, *axis, lin_keys=lin_keys)
+    return (2.0 * numpy.pi) / sym_num
