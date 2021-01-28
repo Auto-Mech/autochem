defea98e8af689bfd4364655fc6a37daf11b4548
Kevin Moore <kmoore@blueslogin2.lcrc.anl.gov>
2021-01-28 12:43:13 -0600

    update rotor calls

--- automol/rotor/__init__.py
+++ automol/rotor/__init__.py
@@ -1,18 +1,21 @@
 """ rotor library
 """
 
+# Rotor
 from automol.rotor._rotor import from_zma
-# from automol.rotor._rotor import from_data
+from automol.rotor._rotor import from_data
 # from automol.rotor._rotor import names
 # from automol.rotor._rotor import axes
 # from automol.rotor._rotor import groups
 # from automol.rotor._rotor import symmetries
 # from automol.rotor._rotor import names_of_list
 
+#  
+
 
 __all__ = [
     'from_zma',
-    # 'from_data'
+    'from_data'
     # 'names',
     # 'axes',
     # 'groups',
