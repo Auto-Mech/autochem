71ab6c858ee6d575a672b5004a79b02088e6d8e4
Kevin Moore <kmoore@b459.lcrc.anl.gov>
2021-02-23 10:07:04 -0600

    update

--- automol/tests/test_reac.py
+++ automol/tests/test_reac.py
@@ -1180,3 +1180,17 @@ def test__prod__homolytic_scission():
 
 if __name__ == '__main__':
     # test__reac__string()
+    # test__reac__hydrogen_migration()
+    # test__reac__ring_forming_scission()
+    test__reac__hydrogen_abstraction()
+    # test__reac__insertion()
+    # test__reac__substitution()
+    test__reac__sigma_hydrogen_abstraction()
+    test__reac__hydrogen_migration()
+    test__reac__beta_scission()
+    test__reac__ring_forming_scission()
+    test__reac__elimination()
+    test__reac__hydrogen_abstraction()
+    test__reac__addition()
+    test__reac__insertion()
+    test__reac__substitution()
