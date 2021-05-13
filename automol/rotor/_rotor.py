41b058cf81225d3d06069b53f8a3696dba9386c4
snelliott <sarah.elliott26@uga.edu>
2021-05-13 18:01:16 -0500

    _rotor had autofile call in __main__

--- automol/rotor/_rotor.py
+++ automol/rotor/_rotor.py
@@ -16,7 +16,6 @@ from automol.rotor import _tors as tors
 from automol.rotor._name import group_torsions_into_rotors
 from automol.rotor._util import graph_with_keys
 from automol.rotor._util import sort_tors_names
-import autofile.io_
 
 
 # constructors
@@ -232,30 +231,31 @@ if __name__ == '__main__':
 
     # print(automol.geom.string(GEO))
 
-    GEO_STR = autofile.io_.read_file('zmat.xyz')
-    RXN_STR = autofile.io_.read_file('zmat.r.yaml')
+    # with open('zmat.xyz', 'r') as f:
+    #     GEO_STR = f.read()
+    # with open('zmat.r.yaml', 'r') as f:
+    #     RXN_STR = f.read()
+    # GEO = autofile.data_types.sread.geometry(GEO_STR)
+    # RXN = autofile.data_types.sread.reaction(RXN_STR)
 
-    GEO = autofile.data_types.sread.geometry(GEO_STR)
-    RXN = autofile.data_types.sread.reaction(RXN_STR)
+    # ZMA, ZMA_KEYS, DUMMY_KEY_DCT = automol.reac.ts_zmatrix(RXN, GEO)
 
-    ZMA, ZMA_KEYS, DUMMY_KEY_DCT = automol.reac.ts_zmatrix(RXN, GEO)
+    # ZRXN = automol.reac.relabel_for_zmatrix(RXN, ZMA_KEYS, DUMMY_KEY_DCT)
+    # ZTSG = ZRXN.forward_ts_graph
 
-    ZRXN = automol.reac.relabel_for_zmatrix(RXN, ZMA_KEYS, DUMMY_KEY_DCT)
-    ZTSG = ZRXN.forward_ts_graph
+    # print('zma:\n', automol.zmat.string(ZMA))
 
-    print('zma:\n', automol.zmat.string(ZMA))
+    # LIN_KEYS = sorted(
+    #     automol.graph.dummy_atoms_neighbor_atom_key(ZTSG).values())
+    # BND_KEYS = automol.graph.rotational_bond_keys(ZTSG, lin_keys=LIN_KEYS)
+    # print(LIN_KEYS)
+    # print(BND_KEYS)
 
-    LIN_KEYS = sorted(
-        automol.graph.dummy_atoms_neighbor_atom_key(ZTSG).values())
-    BND_KEYS = automol.graph.rotational_bond_keys(ZTSG, lin_keys=LIN_KEYS)
-    print(LIN_KEYS)
-    print(BND_KEYS)
+    # NAMES = [automol.zmat.torsion_coordinate_name(ZMA, *k) for k in BND_KEYS]
+    # print(NAMES)
 
-    NAMES = [automol.zmat.torsion_coordinate_name(ZMA, *k) for k in BND_KEYS]
-    print(NAMES)
-
-    ROTORS = from_zmatrix(ZMA, zrxn=ZRXN)
-    print(ROTORS)
+    # ROTORS = from_zmatrix(ZMA, zrxn=ZRXN)
+    # print(ROTORS)
 
     # ZMA_STR = autofile.io_.read_file('zmat.zmat')
     # ZMA = autofile.data_types.sread.zmatrix(ZMA_STR)
