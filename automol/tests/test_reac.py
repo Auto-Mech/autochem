6318be16f6bbb6383cc704eef8ea61101e7eea1b
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2021-06-16 21:43:44 -0500

    clean and update tests

--- automol/tests/test_reac.py
+++ automol/tests/test_reac.py
@@ -272,7 +272,11 @@ def test__reac__reagents_graph():
     rxn = automol.reac.from_string(SUBSTITUTION_RXN_STR)
 
     rcts_gra = automol.reac.reactants_graph(rxn)
+<<<<<<< HEAD
     prds_gra = automol.reac.products_graph(rxn)
+=======
+    prds_gra = automol.reac.reactants_graph(rxn)
+>>>>>>> clean and update tests
 
     assert rcts_gra == (
         {0: ('O', 0, None), 1: ('C', 0, None), 2: ('H', 0, None),
@@ -303,6 +307,16 @@ def test__reac__reagents_graph():
 
     assert prds_gra == automol.reac.reactants_graph(rxn, rev=True)
 
+<<<<<<< HEAD
+=======
+
+def test__check_radrad():
+    """ test automol.reac.is_radical_radical
+        test automol.reac.is_barrierless
+    """
+    pass
+
+>>>>>>> clean and update tests
 
 def test__reac__hydrogen_migration():
     """ test hydrogen migration functionality
@@ -313,6 +327,9 @@ def test__reac__hydrogen_migration():
     rxn_objs = automol.reac.rxn_objs_from_smiles(rct_smis, prd_smis)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> clean and update tests
     ref_scan_names = ('R2',)
     ref_constraint_dct = {'R1': 2.65}
     ref_scan_grid = (numpy.array([
@@ -323,6 +340,7 @@ def test__reac__hydrogen_migration():
     ref_update_guess = True
     ref_tors_names = {'D9'}
     ref_tors_symms = [3]
+<<<<<<< HEAD
 
     _check_reaction(rxn_objs[0],
                     ref_scan_names, ref_constraint_dct,
@@ -366,6 +384,13 @@ def test__reac__hydrogen_migration():
     sym_nums = [
         automol.reac.rotational_symmetry_number(grxn, *a) for a in axes]
     assert sym_nums == [3]
+>>>>>>> clean and update tests
+=======
+
+    _check_reaction(rxn_objs[0],
+                    ref_scan_names, ref_constraint_dct,
+                    ref_scan_grid, ref_update_guess,
+                    ref_tors_names, ref_tors_symms)
 >>>>>>> clean and update tests
 
 
@@ -420,6 +445,7 @@ def test__reac__beta_scission():
     prd_smis = ['[O][O]', 'CC[CH2]']
     rxn_objs = automol.reac.rxn_objs_from_smiles(rct_smis, prd_smis)
 <<<<<<< HEAD
+<<<<<<< HEAD
 
     ref_scan_names = ('R8',)
     ref_constraint_dct = None
@@ -466,15 +492,31 @@ def test__reac__beta_scission():
     old_zrxn = zrxn
     zrxn = automol.reac.insert_dummy_atoms(grxn, gdummy_key_dct)
     assert zrxn == old_zrxn
+=======
+>>>>>>> clean and update tests
 
-    gbnd_keys = automol.reac.rotational_bond_keys(grxn)
-    assert len(gbnd_keys) == len(bnd_keys)
+    ref_scan_names = ('R8',)
+    ref_constraint_dct = None
+    ref_scan_grid = (numpy.array([
+        2.89128097, 2.99303546, 3.09478994, 3.19654442, 3.29829891,
+        3.40005339, 3.50180787, 3.60356236, 3.70531684, 3.80707133,
+        3.90882581, 4.01058029, 4.11233478, 4.21408926]),)
+    ref_update_guess = False
+    ref_tors_names = {'D11', 'D5', 'D8'}
+    ref_tors_symms = [3, 1, 1]
 
+<<<<<<< HEAD
     axes = sorted(map(sorted, gbnd_keys))
     groups_lst = [automol.reac.rotational_groups(grxn, *a) for a in axes]
     sym_nums = [
         automol.reac.rotational_symmetry_number(grxn, *a) for a in axes]
     assert sym_nums == [3, 1, 1]
+>>>>>>> clean and update tests
+=======
+    _check_reaction(rxn_objs[0],
+                    ref_scan_names, ref_constraint_dct,
+                    ref_scan_grid, ref_update_guess,
+                    ref_tors_names, ref_tors_symms)
 >>>>>>> clean and update tests
 
 
@@ -486,14 +528,24 @@ def test__reac__ring_forming_scission():
     prd_smis = ['C1CCCO1', '[OH]']
     rxn_objs = automol.reac.rxn_objs_from_smiles(rct_smis, prd_smis)
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     rxn, geo, _, _ = rxn_objs[0]
+=======
+>>>>>>> clean and update tests
 
-    # reaction object aligned to z-matrix keys
-    # (for getting torsion coordinate names)
-    zma, zma_keys, dummy_key_dct = automol.reac.ts_zmatrix(rxn, geo)
-    zrxn = automol.reac.relabel_for_zmatrix(rxn, zma_keys, dummy_key_dct)
+    ref_scan_names = ('R13',)
+    ref_constraint_dct = {'A4': 1.85, 'A7': 2.15, 'A10': 1.91,
+                          'D7': 6.28, 'D10': 0.01, 'D13': 3.15}
+    ref_scan_grid = ((
+        2.834589188186742, 3.0235618007325247, 3.212534413278308,
+        3.4015070258240905, 3.590479638369873, 3.7794522509156563,
+        3.968424863461439),)
+    ref_update_guess = False
+    ref_tors_names = {'D14'}
+    ref_tors_symms = [1]
 
+<<<<<<< HEAD
     # You can also do this to determine linear atoms from zmatrix:
     # bnd_keys = automol.reac.rotational_bond_keys(zrxn, zma=zma)
     bnd_keys = automol.reac.rotational_bond_keys(zrxn)
@@ -532,6 +584,8 @@ def test__reac__ring_forming_scission():
     ref_tors_names = {'D14'}
     ref_tors_symms = [1]
 
+=======
+>>>>>>> clean and update tests
     _check_reaction(rxn_objs[0],
                     ref_scan_names, ref_constraint_dct,
                     ref_scan_grid, ref_update_guess,
@@ -553,7 +607,10 @@ def test__reac__elimination():
             3.77045681, 4.09440986, 4.41836291, 4.74231596, 5.06626901,
             5.39022206, 5.71417511, 6.03812816]),
         numpy.array([3.35680094, 4.07101391, 4.78522687, 5.49943984])),)
+<<<<<<< HEAD
     # ^ correct grid shape?
+=======
+>>>>>>> clean and update tests
     ref_update_guess = False
     ref_tors_names = {'D9'}
     ref_tors_symms = [3]
@@ -669,6 +726,7 @@ def test__reac__radrad_addition():
     prd_smis = ['CCC']
 
     rxn_objs = automol.reac.rxn_objs_from_smiles(rct_smis, prd_smis)
+<<<<<<< HEAD
 
     ref_scan_names = ('R10',)
     ref_constraint_dct = None
@@ -677,6 +735,19 @@ def test__reac__radrad_addition():
     ref_tors_names = {'D7', 'D4'}
     ref_tors_symms = [3, 3]
 
+=======
+
+    ref_scan_names = ('R10',)
+    ref_constraint_dct = None
+    ref_scan_grid = (numpy.array([
+        2.24877409, 2.29877409, 2.35377409, 2.41427409, 2.48082409,
+        2.55402909, 2.63455459, 2.72313264, 2.82056849, 2.92774793,
+        3.04564532, 3.17533244, 3.31798828, 3.4749097, 3.64752326]),)
+    ref_update_guess = False
+    ref_tors_names = {'D7', 'D4'}
+    ref_tors_symms = [3, 3]
+
+>>>>>>> clean and update tests
     _check_reaction(rxn_objs[0],
                     ref_scan_names, ref_constraint_dct,
                     ref_scan_grid, ref_update_guess,
@@ -693,7 +764,13 @@ def test__reac__radrad_hydrogen_abstraction():
 
     ref_scan_names = ('R12',)
     ref_constraint_dct = None
+<<<<<<< HEAD
     ref_scan_grid = None  # not calling the radrad builder
+=======
+    ref_scan_grid = (numpy.array([
+        1.58736995, 1.83033473, 2.07329952, 2.31626431, 2.5592291,
+        2.80219388, 3.04515867, 3.28812346]),)
+>>>>>>> clean and update tests
     ref_update_guess = False
     ref_tors_names = {'D8', 'D5'}
     ref_tors_symms = [3, 1]
@@ -965,7 +1042,11 @@ def _check_reaction(rxn_obj,
 
     # Get scan information
     scan_info = automol.reac.build_scan_info(zrxn, zma)
+<<<<<<< HEAD
     scan_names, constraint_dct, scan_grid, update_guess = scan_info
+=======
+    scan_names, constraint_dct, grid, update_guess = scan_info
+>>>>>>> clean and update tests
 
     # graph aligned to geometry keys
     # (for getting rotational groups and symmetry numbers)
@@ -988,6 +1069,7 @@ def _check_reaction(rxn_obj,
     if ref_scan_names is not None:
         assert scan_names == ref_scan_names
     if ref_constraint_dct is not None:
+<<<<<<< HEAD
         assert set(constraint_dct.keys()) == set(ref_constraint_dct.keys())
     if ref_scan_grid is not None:
         for rgrd, grd in zip(ref_scan_grid, scan_grid):
@@ -996,6 +1078,11 @@ def _check_reaction(rxn_obj,
             else:
                 for sub_rgrd, sub_grd in zip(rgrd, grd):
                     assert(numpy.allclose(sub_rgrd, sub_grd))
+=======
+        assert constraint_dct == ref_constraint_dct
+    if ref_scan_grid is not None:
+        assert grid == ref_scan_grid
+>>>>>>> clean and update tests
     if ref_update_guess is not None:
         assert update_guess == ref_update_guess
     if ref_tors_names is not None:
