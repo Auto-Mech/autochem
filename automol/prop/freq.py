06ea70034f7fb5b3dafc4911c1054cd2338a2017
snelliott <sarah.elliott26@uga.edu>
2021-06-28 13:58:22 -0500

    harmonic scaling

--- automol/prop/freq.py
+++ automol/prop/freq.py
@@ -5,24 +5,29 @@ import numpy
 from phydat import phycon
 
 
-def anharm_by_scaling(freqs, method, basis, scale_method='c3'):
+def scale_frequencies_and_zpe(freqs, method, basis, scale_method='c3'):
     """ Scale frequencies according to some method
         obtain a corrected zpe
     """
-
-    # Scale the frequencies
-    if scale_method in SCALE_METHODS:
-        scaled_freqs = SCALE_METHODS[scale_method](freqs, method, basis)
-    else:
-        scaled_freqs = freqs
-
+    scaled_freqs = scale_frequencies(
+        freqs,  method, basis, scale_method=scale_method)
     # Calculate the anharmonic zpe using the scaled freqs (anharm zpve)
     scaled_zpe = 0.0
     for freq, scfreq in zip(freqs, scaled_freqs):
         scaled_zpe += _anharm_zpve_from_scaling(freq, scfreq)
     scaled_zpe *= phycon.WAVEN2EH
-
     return scaled_freqs, scaled_zpe
+
+
+def scale_frequencies(freqs, method, basis, scale_method='c3'):
+    """ Scale frequencies according to some method
+    """
+    # Scale the frequencies
+    if scale_method in SCALE_METHODS:
+        scaled_freqs = SCALE_METHODS[scale_method](freqs, method, basis)
+    else:
+        scaled_freqs = freqs
+    return scaled_freqs
 
 
 def _anharm_zpve_from_scaling(freq, scaled_freq):
@@ -79,17 +84,21 @@ def rotor_scale_factor_from_harmonics(rt_freqs, rth_freqs, tors_freqs):
 
 
 # Library of vibrational frequency scaling methods
-M3_COEFFS = {
+M3_COEFFS_ANHARM = {
     ('b2plypd3', 'cc-pvtz'): (1.066, 0.008045, 0.33),
     ('wb97xd', '6-31g*'): (1.657244, 0.56000691, 0.029624)
 }
 
+M3_COEFFS_HARM = {
+    ('wb97xd', '6-31g*'): (0.91, -0.058, 0.001)
+}
 
-def _three_coeff_scaling(freqs, method, basis):
+
+def _three_coeff_anharm_scaling(freqs, method, basis):
     """ Scales frequencies using factos with three coefficients
     """
 
-    cf1, cf2, cf3 = M3_COEFFS.get((method, basis), (1.0, 0.0, 0.0))
+    cf1, cf2, cf3 = M3_COEFFS_ANHARM.get((method, basis), (1.0, 0.0, 0.0))
     scaled_freqs = ()
     for freq in freqs:
         scale_factor = cf1 - (cf2 * freq**cf3)
@@ -98,6 +107,18 @@ def _three_coeff_scaling(freqs, method, basis):
     return scaled_freqs
 
 
+def _three_coeff_harm_scaling(freqs, method, basis):
+    """ Scales frequencies using one factor, same factor applies to all frequencies
+    """
+    cf1, cf2, cf3 = M3_COEFFS_HARM.get((method, basis), (1.0, 0.0, 0.0))
+    scaled_freqs = ()
+    for freq in freqs:
+        scale_factor = cf1 - (cf2 * freq**cf3)
+        scaled_freqs += (freq * scale_factor,)
+    return scaled_freqs
+
+
 SCALE_METHODS = {
-    'c3': _three_coeff_scaling
+    'c3': _three_coeff_anharm_scaling,
+    'c3_harm':  _three_coeff_harm_scaling
 }
