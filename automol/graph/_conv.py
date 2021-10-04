1002fc1ed6edc51e470ca90e73d39bb1ab83489d
avcopan <avcopan@gmail.com>
2021-10-04 16:45:08 -0400

    Enable molfile generation

--- automol/graph/_conv.py
+++ automol/graph/_conv.py
@@ -109,6 +109,7 @@ def inchi_with_sort_from_geometry(gra, geo=None, geo_idx_dct=None):
                                                  geo_idx_dct=geo_idx_dct)
     rdm = rdkit_.from_molfile(mlf)
     ich, aux_info = rdkit_.to_inchi(rdm, with_aux_info=True)
+
     nums_lst = _parse_sort_order_from_aux_info(aux_info)
     nums_lst = tuple(tuple(map(key_map_inv.__getitem__, nums))
                      for nums in nums_lst)
