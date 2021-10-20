a0a6eb2e094ff2c9d181c7dc249470fd03c2049c
Kevin Moore <kmoore@blueslogin1.lcrc.anl.gov>
2021-10-20 14:48:44 -0500

    update conda files

--- setup.py
+++ setup.py
@@ -6,41 +6,45 @@ from distutils.core import setup
 
 setup(
     name='autochem',
-    version='0.8.2',
-    packages=['automol',
-              # L1
-              'automol.util',
-              'automol.util.dict_',
-              'automol.mult',
-              'automol.formula',
-              'automol.prop',
-              'automol.embed',
-              # L2
-              'automol.geom.base',
-              'automol.graph.base',
-              'automol.zmat.base',
-              # L3
-              'automol.extern',
-              'automol.inchi.base',
-              # L4
-              'automol.graph',
-              'automol.geom',
-              'automol.inchi',
-              'automol.zmat',
-              # L5
-              'automol.pot',
-              'automol.etrans',
-              'automol.reac',
-              'automol.rotor',
-              # other
-              'autoreact',
-              'phydat',
-              'transformations'],
-    package_dir={'automol': 'automol',
-                 'autoreact': 'autoreact',
-                 'phydat': 'phydat',
-                 'transformations': 'transformations'
+    version='0.9.0',
+    packages=[
+        'automol',
+        # L1
+        'automol.util',
+        'automol.util.dict_',
+        'automol.mult',
+        'automol.formula',
+        'automol.prop',
+        'automol.embed',
+        # L2
+        'automol.geom.base',
+        'automol.graph.base',
+        'automol.zmat.base',
+        # L3
+        'automol.extern',
+        'automol.inchi.base',
+        # L4
+        'automol.graph',
+        'automol.geom',
+        'automol.inchi',
+        'automol.zmat',
+        # L5
+        'automol.pot',
+        'automol.etrans',
+        'automol.reac',
+        'automol.rotor',
+        # other
+        'autoreact',
+        'phydat',
+        'transformations'
+    ],
+    package_dir={
+        'automol': 'automol',
+        'autoreact': 'autoreact',
+        'phydat': 'phydat',
+        'transformations': 'transformations'
     },
-    package_data={'automol': ['tests/data/*'],
+    package_data={
+        'automol': ['tests/data/*'],
     }
 )
