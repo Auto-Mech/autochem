89ae512a09374bac475876fa11488aadd236bb9b
Kevin Moore <kmoore@blueslogin2.lcrc.anl.gov>
2021-11-09 12:18:16 -0600

    add inchi-conv debug print

--- automol/inchi/_conv.py
+++ automol/inchi/_conv.py
@@ -140,8 +140,7 @@ def _connected_geometry(ich, check=True):
                 continue
 
         if not success:
-            print('Failed InChI:', ich)
-            raise error.FailedGeometryGenerationError
+            raise error.FailedGeometryGenerationError('Failed InChI:', ich)
 
     return geo
 
