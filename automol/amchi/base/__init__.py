a3c6de820729acc90e21265676f6db31d532af56
avcopan <avcopan@gmail.com>
2021-12-14 16:26:34 -0500

    Fills in AMChI core functions

--- automol/amchi/base/__init__.py
+++ automol/amchi/base/__init__.py
@@ -3,9 +3,55 @@
 
 # # constructor
 from automol.amchi.base._core import from_data
+# # getters
+from automol.amchi.base._core import prefix
+from automol.amchi.base._core import version
+from automol.amchi.base._core import formula_string
+from automol.amchi.base._core import main_layers
+from automol.amchi.base._core import charge_layers
+from automol.amchi.base._core import stereo_layers
+from automol.amchi.base._core import isotope_layers
+# # conversions
+from automol.amchi.base._core import formula
+# # properties
+from automol.amchi.base._core import is_chiral
+from automol.amchi.base._core import is_enantiomer
+from automol.amchi.base._core import has_stereo
+from automol.amchi.base._core import has_multiple_components
+from automol.amchi.base._core import charge
+from automol.amchi.base._core import low_spin_multiplicity
+from automol.amchi.base._core import stereo_atoms
+from automol.amchi.base._core import stereo_bonds
+from automol.amchi.base._core import unassigned_stereo_bonds
+# # split/join
+from automol.amchi.base._core import split
+from automol.amchi.base._core import join
 
 
 __all__ = [
     # # constructor
     'from_data',
+    # # getters
+    'prefix',
+    'version',
+    'formula_string',
+    'main_layers',
+    'charge_layers',
+    'stereo_layers',
+    'isotope_layers',
+    # # conversions
+    'formula',
+    # # properties
+    'is_chiral',
+    'is_enantiomer',
+    'has_stereo',
+    'has_multiple_components',
+    'charge',
+    'low_spin_multiplicity',
+    'stereo_atoms',
+    'stereo_bonds',
+    'unassigned_stereo_bonds',
+    # # split/join
+    'split',
+    'join',
 ]
