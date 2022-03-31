43bb7a113ae91ce741819094c952b64396aa1195
avcopan <avcopan@gmail.com>
2022-03-31 14:35:59 -0400

    Working on conversions. AMChI bug fix for certain rings

--- automol/rsmiles/base/__init__.py
+++ automol/rsmiles/base/__init__.py
@@ -1,10 +1,16 @@
 """ Level 2 RSMILES functions
 """
 
+# # split/join
+from automol.rsmiles.base._core import split
+from automol.rsmiles.base._core import join
 # # properties
-from automol.rsmiles.base._core import parse_properties
+from automol.rsmiles.base._core import parse_connected_molecule_properties
 
 __all__ = [
+    # # split/join
+    'split',
+    'join',
     # # properties
-    'parse_properties',
+    'parse_connected_molecule_properties',
 ]
