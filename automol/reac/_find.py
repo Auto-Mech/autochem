57bb8c2b3bc653b95cb4267f2373f5a8b63c6acb
avcopan <avcopan@gmail.com>
2023-04-08 15:25:51 -0400

    Rename function

--- automol/reac/_find.py
+++ automol/reac/_find.py
@@ -696,16 +696,16 @@ def find(rct_gras, prd_gras):
     return rxns
 
 
-def find_from_inchis(rct_ichs, prd_ichs):
+def find_from_chi(rct_chis, prd_chis):
     """ find all reaction classes consistent with these reactants and products
 
-    :param rct_ichs: inchis for the reactants
-    :param prd_ichs: inchis for the products
+    :param rct_chis: inchis for the reactants
+    :param prd_chis: inchis for the products
     :returns: a list of reaction classes
     :rtype: tuple[str]
     """
-    rct_geos = list(map(automol.chi.geometry, rct_ichs))
-    prd_geos = list(map(automol.chi.geometry, prd_ichs))
+    rct_geos = list(map(automol.chi.geometry, rct_chis))
+    prd_geos = list(map(automol.chi.geometry, prd_chis))
     rct_gras = list(map(automol.geom.connectivity_graph, rct_geos))
     prd_gras = list(map(automol.geom.connectivity_graph, prd_geos))
     rct_gras, _ = automol.graph.standard_keys_for_sequence(rct_gras)
