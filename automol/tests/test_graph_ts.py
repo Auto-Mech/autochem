0821ebc11e8a42722f6aa862ac60cebce187ef15
Andreas Copan <avcopan@gmail.com>
2023-06-01 15:12:24 -0400

    Extends has_stereo to detect extra TS stereo by request

--- automol/tests/test_graph_ts.py
+++ automol/tests/test_graph_ts.py
@@ -30,6 +30,80 @@ C4H9O3_TSG = ({0: ('C', 0, None, None, None), 1: ('C', 0, None, None, None),
                frozenset({0, 8}): (1, None, None, None),
                frozenset({0, 9}): (1, None, None, None)})
 
+# CCOCC + [OH] => C[CH]OCC + O
+#  *
+# [* marks a fleeting TS stereosite]
+C4H11O2_TSG = ({0: ('C', 0, None, None, None), 1: ('C', 0, None, None, None),
+                2: ('C', 0, None, None, True), 3: ('C', 0, None, None, None),
+                4: ('O', 0, None, None, None), 5: ('H', 0, None, None, None),
+                6: ('H', 0, None, None, None), 7: ('H', 0, None, None, None),
+                8: ('H', 0, None, None, None), 9: ('H', 0, None, None, None),
+                10: ('H', 0, None, None, None), 11: ('H', 0, None, None, None),
+                12: ('H', 0, None, None, None), 13: ('H', 0, None, None, None),
+                14: ('H', 0, None, None, None), 15: ('O', 0, None, None, None),
+                16: ('H', 0, None, None, None)},
+               {frozenset({0, 5}): (1, None, None, None),
+                frozenset({3, 14}): (1, None, None, None),
+                frozenset({11, 15}): (0.1, None, None, None),
+                frozenset({2, 11}): (0.9, None, None, None),
+                frozenset({1, 10}): (1, None, None, None),
+                frozenset({3, 4}): (1, None, None, None),
+                frozenset({1, 9}): (1, None, None, None),
+                frozenset({0, 6}): (1, None, None, None),
+                frozenset({0, 2}): (1, None, None, None),
+                frozenset({3, 13}): (1, None, None, None),
+                frozenset({2, 12}): (1, None, None, None),
+                frozenset({15, 16}): (1, None, None, None),
+                frozenset({2, 4}): (1, None, None, None),
+                frozenset({1, 8}): (1, None, None, None),
+                frozenset({0, 7}): (1, None, None, None),
+                frozenset({1, 3}): (1, None, None, None)})
+
+# C=C(O[O])OO => [CH]=C(OO)OO
+#  *
+# [* marks a fleeting TS stereosite]
+C2H3O4_TSG = ({0: ('C', 0, None, None, None), 1: ('C', 0, None, None, None),
+               2: ('O', 0, None, None, None), 3: ('O', 0, None, None, None),
+               4: ('O', 0, None, None, None), 5: ('O', 0, None, None, None),
+               6: ('H', 0, None, None, None), 7: ('H', 0, None, None, None),
+               8: ('H', 0, None, None, None)},
+              {frozenset({2, 8}): (1, None, None, None),
+               frozenset({1, 4}): (1, None, None, None),
+               frozenset({0, 6}): (0.9, None, None, None),
+               frozenset({0, 1}): (1, None, None, True),
+               frozenset({3, 6}): (0.1, None, None, None),
+               frozenset({2, 4}): (1, None, None, None),
+               frozenset({1, 5}): (1, None, None, None),
+               frozenset({3, 5}): (1, None, None, None),
+               frozenset({0, 7}): (1, None, None, None)})
+
+
+# # F/C=C([C@@H](F)O)\[C@H](F)O + [OH] => F[C@H]([C]([C@@H](F)O)[C@H](F)O)O
+# C4H5F3O2_TSG = (
+#     {0: ('C', 0, None, False, None), 1: ('C', 0, None, None, None),
+#      2: ('C', 0, False, False, None), 3: ('C', 0, True, True, None),
+#      4: ('F', 0, None, None, None), 5: ('F', 0, None, None, None),
+#      6: ('F', 0, None, None, None), 7: ('O', 0, None, None, None),
+#      8: ('O', 0, None, None, None), 9: ('H', 0, None, None, None),
+#      10: ('H', 0, None, None, None), 11: ('H', 0, None, None, None),
+#      12: ('H', 0, None, None, None), 13: ('H', 0, None, None, None),
+#      14: ('O', 0, None, None, None), 15: ('H', 0, None, None, None)},
+#     {frozenset({7, 12}): (1, None, None, None),
+#      frozenset({2, 10}): (1, None, None, None),
+#      frozenset({1, 2}): (1, None, None, None),
+#      frozenset({0, 1}): (1, False, None, None),
+#      frozenset({3, 6}): (1, None, None, None),
+#      frozenset({2, 5}): (1, None, None, None),
+#      frozenset({0, 4}): (1, None, None, None),
+#      frozenset({3, 8}): (1, None, None, None),
+#      frozenset({0, 14}): (0.1, None, None, None),
+#      frozenset({1, 3}): (1, None, None, None),
+#      frozenset({8, 13}): (1, None, None, None),
+#      frozenset({14, 15}): (1, None, None, None),
+#      frozenset({3, 11}): (1, None, None, None),
+#      frozenset({2, 7}): (1, None, None, None),
+#      frozenset({0, 9}): (1, None, None, None)})
+
 
 def test__from_data():
     """ test getters
@@ -171,6 +245,29 @@ def test__string():
     assert gra == graph.from_string(graph.string(gra))
 
 
+def test__has_stereo():
+    """ test graph.has_stereo functions
+    """
+    # CCOCC + [OH] => C[CH]OCC + O
+    #  *
+    # [* marks a fleeting TS stereosite]
+    assert not graph.has_stereo(C4H11O2_TSG)
+    assert graph.has_stereo(C4H11O2_TSG, ts_all=True)
+    assert not graph.has_atom_stereo(C4H11O2_TSG)
+    assert graph.has_atom_stereo(C4H11O2_TSG, ts_all=True)
+    assert not graph.has_bond_stereo(C4H11O2_TSG)
+    assert not graph.has_bond_stereo(C4H11O2_TSG, ts_all=True)
+    # C=C(O[O])OO => [CH]=C(OO)OO
+    #  *
+    # [* marks a fleeting TS stereosite]
+    assert not graph.has_stereo(C2H3O4_TSG)
+    assert graph.has_stereo(C2H3O4_TSG, ts_all=True)
+    assert not graph.has_atom_stereo(C2H3O4_TSG)
+    assert not graph.has_atom_stereo(C2H3O4_TSG, ts_all=True)
+    assert not graph.has_bond_stereo(C2H3O4_TSG)
+    assert graph.has_bond_stereo(C2H3O4_TSG, ts_all=True)
+
+
 def test__atoms_neighbor_atom_keys():
     """ test graph.atoms_neighbor_atom_keys
     """
@@ -262,7 +359,6 @@ def test__atoms_bond_keys():
     }
 
 
-# # bond properties
 def test__bonds_neighbor_atom_keys():
     """ test graph.bonds_neighbor_atom_keys
     """
@@ -312,3 +408,4 @@ if __name__ == '__main__':
     test__atoms_neighbor_atom_keys()
     test__atoms_bond_keys()
     test__bonds_neighbor_atom_keys()
+    test__has_stereo()
