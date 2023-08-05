ebf52990438404a39bfab550584d926b3eb0f004
Andreas Copan <avcopan@gmail.com>
2023-08-05 18:27:00 -0400

    Extra functions for formula sorting

--- automol/formula/__init__.py
+++ automol/formula/__init__.py
@@ -2,10 +2,12 @@
 """
 from automol.formula._formula import electron_count
 from automol.formula._formula import atom_count
+from automol.formula._formula import heavy_atom_count
 from automol.formula._formula import element_count
 from automol.formula._formula import add_element
 from automol.formula._formula import join
 from automol.formula._formula import join_sequence
+from automol.formula._formula import sorted_symbols_in_sequence
 from automol.formula._formula import string
 from automol.formula._formula import string2
 from automol.formula._formula import from_string
@@ -18,10 +20,12 @@ from automol.formula import reac
 __all__ = [
     'electron_count',
     'atom_count',
+    'heavy_atom_count',
     'element_count',
     'add_element',
     'join',
     'join_sequence',
+    'sorted_symbols_in_sequence',
     'string',
     'string2',
     'from_string',
