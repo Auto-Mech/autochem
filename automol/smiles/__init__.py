6863bbf7b6994e91788608c4035246224b8c7fc6
Andreas Copan <avcopan@gmail.com>
2023-08-05 10:20:44 -0400

    SMILES functions to remove stereo and get formula

--- automol/smiles/__init__.py
+++ automol/smiles/__init__.py
@@ -5,6 +5,7 @@ SMILES, with an extension for resonance double-bond stereo.
 # L2
 # # conversions
 from automol.smiles.base._core import without_resonance_stereo
+from automol.smiles.base._core import without_stereo
 from automol.smiles.base._core import reflect
 # # split/join
 from automol.smiles.base._core import split
@@ -19,6 +20,8 @@ from automol.smiles._conv import inchi
 from automol.smiles._conv import chi
 from automol.smiles._conv import graph
 from automol.smiles._conv import geometry
+from automol.smiles._conv import formula_string
+from automol.smiles._conv import recalculate_without_stereo
 from automol.smiles._conv import rdkit_molecule
 from automol.smiles._conv import rdkit_reaction
 from automol.smiles._conv import display
@@ -29,6 +32,7 @@ __all__ = [
     # L2
     # # conversions
     'without_resonance_stereo',
+    'without_stereo',
     'reflect',
     # # split/join
     'split',
@@ -43,6 +47,8 @@ __all__ = [
     'chi',
     'graph',
     'geometry',
+    'formula_string',
+    'recalculate_without_stereo',
     'rdkit_molecule',
     'rdkit_reaction',
     'display',
