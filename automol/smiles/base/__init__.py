213318a8326afbf4d20ae7877a27ea9f2dc27de3
Andreas Copan <avcopan@gmail.com>
2023-08-18 13:29:21 -0400

    Adds some basic reaction SMILES parsers

--- automol/smiles/base/__init__.py
+++ automol/smiles/base/__init__.py
@@ -9,6 +9,12 @@ from automol.smiles.base._core import reflect
 from automol.smiles.base._core import split
 from automol.smiles.base._core import join
 from automol.smiles.base._core import reaction
+from automol.smiles.base._core import is_reaction
+from automol.smiles.base._core import reaction_reagents
+from automol.smiles.base._core import reaction_reactant
+from automol.smiles.base._core import reaction_product
+from automol.smiles.base._core import reaction_reactants
+from automol.smiles.base._core import reaction_products
 # # properties
 from automol.smiles.base._core import parse_connected_molecule_properties
 
@@ -21,6 +27,12 @@ __all__ = [
     'split',
     'join',
     'reaction',
+    'is_reaction',
+    'reaction_reagents',
+    'reaction_reactant',
+    'reaction_product',
+    'reaction_reactants',
+    'reaction_products',
     # # properties
     'parse_connected_molecule_properties',
 ]
