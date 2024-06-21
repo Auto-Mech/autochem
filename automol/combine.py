91d2dfa1e0ba0feb5eed6eda7a1ae553311fff30
avcopan <avcopan@gmail.com>
2024-06-21 14:52:28 -0500

    New: Combination of 3+ species

--- automol/combine.py
+++ automol/combine.py
@@ -4,6 +4,10 @@
       (2) reaction path points
 """
 
+import functools
+import itertools
+from operator import mul
+
 from automol import form, geom
 
 
@@ -19,7 +23,7 @@ def fake_vdw_geometry(*geos):
     return geom.join_sequence(geos)
 
 
-def fake_vdw_frequencies_new(*geos):
+def fake_vdw_frequencies(*geos):
     """generate fake frequencies to fill in the missing internal modes of a fake well"""
 
     def _external_mode_count(geo):
@@ -27,59 +31,28 @@ def fake_vdw_frequencies_new(*geos):
 
     fake_geo = fake_vdw_geometry(*geos)
     nfake = sum(map(_external_mode_count, geos)) - _external_mode_count(fake_geo)
-    return tuple(30.0 + i * 20.0 for i in range(nfake))
-
-
-def fake_vdw_frequencies(geo1, geo2):
-    """Set fake frequencies"""
-
-    freqs = [30.0, 50.0, 70.0, 100.0, 200.0]
-    ntrans = 5
-
-    # Check atom types
-    is_atom_i = geom.is_atom(geo1)
-    is_linear_i = geom.is_linear(geo1)
-    is_atom_j = geom.is_atom(geo2)
-    is_linear_j = geom.is_linear(geo2)
-    if is_atom_i and is_atom_j:
-        ntrans = 0
-    else:
-        if is_atom_i:
-            ntrans -= 3
-        if is_atom_j:
-            ntrans -= 3
-        if is_linear_i:
-            ntrans -= 2
-        if is_linear_j:
-            ntrans -= 2
-
-    return tuple(freqs[0:ntrans])
+    return tuple(10.0 * (i + 1) for i in range(nfake))
 
 
 # Combines by elec levels
-def electronic_energy_levels(elec_levels_i, elec_levels_j):
-    """Put two elec levels together for two species"""
+def electronic_energy_levels(*elec_levels_lst):
+    """Combine electronic levels for multiple species"""
 
-    # Combine the energy levels
-    init_elec_levels = []
-    for _, elec_level_i in enumerate(elec_levels_i):
-        for _, elec_level_j in enumerate(elec_levels_j):
-            init_elec_levels.append(
-                [elec_level_i[0] + elec_level_j[0], elec_level_i[1] * elec_level_j[1]]
-            )
+    def _merge_level_combinations(elec_level_comb):
+        """Merge a combination of fragment electronic levels into one"""
+        enes, mults = zip(*elec_level_comb)
+        return (sum(enes), functools.reduce(mul, mults))
 
-    # See if any levels repeat and thus need to be added together
-    elec_levels = []
-    for level in init_elec_levels:
-        # Put level in in final list
-        if level not in elec_levels:
-            elec_levels.append(level)
-        # Add the level to the one in the list
-        else:
-            idx = elec_levels.index(level)
-            elec_levels[idx][1] += level[1]
+    # 1. Get combinations
+    elec_level_combs = list(itertools.product(*elec_levels_lst))
+    # 2. Combine them
+    elec_levels = tuple(map(_merge_level_combinations, elec_level_combs))
+    # 3. Sort them
+    elec_levels = tuple(sorted(elec_levels, key=lambda x: x[0]))
+    # 4. Combine duplicates
+    # Note: Must account for the increased degeneracy!
+    elec_levels = tuple(
+        (e, m * len(list(g))) for (e, m), g in itertools.groupby(elec_levels)
+    )
 
-    # Sort the levels by the value of the energy
-    elec_levels = sorted(elec_levels, key=lambda x: x[0])
-
-    return tuple(map(tuple, elec_levels))
+    return elec_levels
