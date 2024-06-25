935c63dd79bf6f1c66986ace30df62a21ec97f10
Andrea Della Libera <adellalibera@beboplogin2.lcrc.anl.gov>
2024-06-25 10:40:59 -0500

    Fixed line lengths

--- automol/geom/_ring.py
+++ automol/geom/_ring.py
@@ -37,8 +37,10 @@ def ring_angles_reasonable(geo, ring_atoms, thresh=ATHRESH):
     """
 
     condition = True
-    # Lower threshold for 4 and 5 membered rings if not using relaxed thresh already
-    if len(ring_atoms) < 6 and thresh > 70.: thresh *= 0.8
+    # Lower threshold for 4 and 5 membered rings 
+    # if not using relaxed thresh already
+    if len(ring_atoms) < 6 and thresh > 70.: 
+        thresh *= 0.8
     for i, ring_atom in enumerate(ring_atoms):
         _atoms = [ring_atom, ring_atoms[i - 1], ring_atoms[i - 2]]
         cangle = central_angle(geo, *_atoms, degree=False)
@@ -55,8 +57,7 @@ def ring_fragments_geometry(geo, rings_atoms=None, ngbs=None):
     :param geo: molecular geometry
     :type geo: automol.geom object
     """
-    #adl TODO call with fix_hyper = False to prevent "assertion" error when calling fram_samp_geo
-    gra = graph(geo)  
+    gra = graph(geo)
     if rings_atoms is None:
         rings_atoms = graph_base.rings_atom_keys(gra)
     if ngbs is None:
@@ -79,13 +80,12 @@ def ring_fragments_geometry(geo, rings_atoms=None, ngbs=None):
 
 
 # adl added functions for Cremer Pople parameters
-# Inspired from Chan et al. 2021 https://doi.org/10.1021/acs.jcim.0c01144 
+# Inspired from Chan et al. 2021 https://doi.org/10.1021/acs.jcim.0c01144
 def translate_to_ring_center(coords):
     """Translate reference system to center of the ring
 
     :param coords: cartesian coordinates of ring atoms
     :type coords: list of lists [[x1,y1,z1],[x2,y2,z2],...]
-
     :output new_coords: translated cartesian coordinates of ring atoms
     :type new_coords: np.array 
     """
@@ -97,7 +97,6 @@ def mean_ring_plane(coords):
 
     :param coords: cartesian coordinates of ring atoms, origin on ring center
     :type coords: np.array
-
     :output R1,R2: 3d vectors defining the ring mean plane
     :type R1,R2: np.array  
     """
@@ -111,7 +110,6 @@ def normal_to_ring_plane(coords):
 
     :param coords: cartesian coordinates of ring atoms, origin on ring center
     :type coords: np.array
-
     :output n: normalized vector perpendicular to R1,R2 plane
     :type n: np.array  
     """
@@ -124,8 +122,7 @@ def get_displacement(coords):
 
     :param coords: cartesian coordinates of ring atoms, origin on ring center
     :type coords: np.array
-
-    :output z: vector defining the atoms dispalcements with respect to the ring mean plane
+    :output z: vector of atoms dispalcements with respect to the ring mean plane
     :type z: np.array  
     """
     n = normal_to_ring_plane(coords)
@@ -133,11 +130,10 @@ def get_displacement(coords):
     return z
 
 def cremer_pople_params(coords):
-    """Computes Cremer-Pople parameters for conformational analysis of ring structures.
+    """Computes Cremer-Pople parameters for conformational analysis of ring structures
 
     :param coords: cartesian coordinates of ring atoms
     :type coords: list of lists [[x1,y1,z1],[x2,y2,z2],...]
-
     :output amplitude,angle: set of puckering parameters
     :type amplitude,angle: tuple of lists ([q2,q3,...],[phi2,phi3...])
     """
@@ -149,17 +145,22 @@ def cremer_pople_params(coords):
     z = get_displacement(coords_array)
     if n_atoms%2 == 0: # n_atoms even
         m = range(2,int((n_atoms/2)))
-        cos_component = [np.dot(z,np.cos(2*np.pi*k*np.arange(0,n_atoms)/n_atoms)) for k in m]
-        sin_component = [np.dot(z,np.sin(2*np.pi*k*np.arange(0,n_atoms)/n_atoms)) for k in m]
+        cos_component = [np.dot(z,np.cos(
+            2*np.pi*k*np.arange(0,n_atoms)/n_atoms)) for k in m]
+        sin_component = [np.dot(z,np.sin(
+            2*np.pi*k*np.arange(0,n_atoms)/n_atoms)) for k in m]
         qcos = np.sqrt(2/n_atoms)*np.array(cos_component)
         qsin = -np.sqrt(2/n_atoms)*np.array(sin_component)
         q = np.sqrt(qsin**2 + qcos**2)
-        amplitude = np.append(q, (1/np.sqrt(n_atoms))*np.dot(z,np.cos(np.arange(0,n_atoms)*np.pi)).sum()).tolist()
+        amplitude = np.append(q, (1/np.sqrt(n_atoms))*np.dot(
+            z,np.cos(np.arange(0,n_atoms)*np.pi)).sum()).tolist()
         angle = np.arctan2(qsin,qcos).tolist()
     else: # n_atoms odd
         m = range(2,int((n_atoms-1)/2)+1)
-        cos_component = [np.dot(z,np.cos(2*np.pi*k*np.arange(0,n_atoms)/n_atoms)) for k in m]
-        sin_component = [np.dot(z,np.sin(2*np.pi*k*np.arange(0,n_atoms)/n_atoms)) for k in m]
+        cos_component = [np.dot(z,np.cos(
+            2*np.pi*k*np.arange(0,n_atoms)/n_atoms)) for k in m]
+        sin_component = [np.dot(z,np.sin(
+            2*np.pi*k*np.arange(0,n_atoms)/n_atoms)) for k in m]
         qcos = np.sqrt(2/n_atoms)*np.array(cos_component)
         qsin = -np.sqrt(2/n_atoms)*np.array(sin_component)
         amplitude = np.sqrt(qsin**2 + qcos**2).tolist()
