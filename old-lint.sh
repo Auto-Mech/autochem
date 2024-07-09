56057c2a656f8cf13979c4899137a5f6ac89e7f0
Andreas Copan <avcopan@gmail.com>
2024-07-09 09:37:31 -0400

    Throw error if linting fails

--- old-lint.sh
+++ old-lint.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+set -e  # if any command fails, quit
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 (
     cd $SCRIPT_DIR
     echo "New linting:"
-    bash ./lint.sh
+    source ./lint.sh
 
     echo "Old linting:"
     pylint --rcfile=.pylintrc automol
